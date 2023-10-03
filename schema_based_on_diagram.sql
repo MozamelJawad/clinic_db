e8f047950c4f6798f8fb5201439546111e91268c
Mershark Oheneba Takyi <113654948+mershark@users.noreply.github.com>
2023-10-03 12:42:30 +0000

    Update with treatment table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -9,7 +9,11 @@ CREATE TABLE patients (
 );
 
 -- create treatments table
- 
+ CREATE TABLE treatments (
+     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+     type VARCHAR(100) NOT NULL,
+     name VARCHAR(120) NOT NULL
+);
 
 -- create medical_histories table
 
