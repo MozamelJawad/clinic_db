8c26e8f5c00db377ed886ed4fb0de0578daa9278
Mozamel Jawad <mozameljawad@gmail.com>
2023-10-03 16:50:19 +0430

    Create database and database file

---
+++ schema_based_on_diagram.sql
@@ -0,0 +1,15 @@
+-- create database clinic
+CREATE DATABASE clinic;
+
+-- create patients table
+
+
+-- create treatments table
+ 
+
+-- create medical_histories table
+
+
+-- create invoices table
+
+
